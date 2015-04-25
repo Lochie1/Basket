762ebc55599307c20bc382c3417db781892291d3
Lochie1 <Lochsta.E@hotmail.com>
2015-04-25 11:14:53 +0800

    lolk2

--- app/src/main/java/com/basket/gmail/basket/Basket.java
+++ app/src/main/java/com/basket/gmail/basket/Basket.java
@@ -112,7 +112,7 @@ public class Basket extends Activity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+
     }
 
     @Override
---
+++ app/src/main/java/com/basket/gmail/basket/Reference/Variables.java
@@ -0,0 +1,8 @@
+package com.basket.gmail.basket.Reference;
+
+/**
+ * Created by Lochie on 25/04/2015.
+ */
+public class Variables {
+    public static final String title_colour = "#ff00ff";
+}
